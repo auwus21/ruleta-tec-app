4caa8b19189c38bc2c9b10b1ae910286e81f3412
auwus21 <agusolthoff2002@gmail.com>
2024-09-15 11:10:26 -0300

    Ruleta Tec 5 codigo tirada

--- src/Felicitaciones.jsx
+++ src/Felicitaciones.jsx
@@ -25,7 +25,7 @@ const Felicitaciones = ({ nombre, premio, imagenPremio }) => {
     <div className="felicitaciones-overlay">
       {!mostrarAgradecimiento ? (
         <div className="felicitaciones-container">
-          <h2>¡Felicitaciones!</h2>
+          <h2>¡{premio === "Sera la proxima..." ? "Será la próxima..." : `Felicitaciones ${nombre}`}!</h2>
           <div className="premio-container">
             <img src={imagenPremio} alt={premio} className="imagen-premio" />
             {premio !== "Sera la proxima..." && <p>Ganaste: {premio}</p>}
@@ -36,7 +36,7 @@ const Felicitaciones = ({ nombre, premio, imagenPremio }) => {
         </div>
       ) : (
         <div className="agradecimiento-container">
-          <h2>Muchas Gracias por participar de la ruleta Tec Store Arg</h2>
+          <h2>Muchas Gracias por participar de la ruleta Tec Store</h2>
           <p>
             Para reclamar tu premio comunicate por alguna de nuestras redes
             mandando el código de pedido y tu nombre completo.
